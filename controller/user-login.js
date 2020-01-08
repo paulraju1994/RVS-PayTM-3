f60b93818113b39eb66aeeab62eae632bdeffaa7
Paul Raju <paul.raju@ADRAPIDVALUE.COM>
2020-01-08 14:42:48 +0530

    login and register changes

--- controller/user-login.js
+++ controller/user-login.js
@@ -2,39 +2,42 @@ var route = require('express').Router();
 var admin = require('firebase-admin');
 var db = admin.firestore();
 var helperObject = require('./helper/helper');
-
+/* API triggered on calling api/user/login */
 route.post('/login', async (request, response) => {
     return new Promise(async (resolve, reject) => {
         var loginuser = request.body;
-        var mobileNumber = loginuser.MobileNumber;
-        const userRef = db.collection('Users').doc(JSON.stringify(mobileNumber));
-        let checkExistence = await helperObject.checkDocId(mobileNumber);
-        if (checkExistence) {
-            userRef.get().then((snapshot) => {
-                if (snapshot.exists) {
-                    if (snapshot.data().MobileNumber == loginuser.MobileNumber
-                        && snapshot.data().Password == loginuser.Password
-                        && snapshot.data().IsAdmin == loginuser.IsAdmin && loginuser.IsAdmin == true) {
-                        return resolve({ Message: 'Admin logged in successfully', Status: 'SUCCESS' });
-                    } else if (snapshot.data().MobileNumber == loginuser.MobileNumber
-                        && snapshot.data().Password == loginuser.Password
-                        && snapshot.data().IsAdmin == loginuser.IsAdmin && loginuser.IsAdmin == false) {
-                        return resolve({ Message: 'User logged in successfully', Status: 'SUCCESS' });
+        /* Check for mandatory fields */
+        if (loginuser.MobileNumber && loginuser.Password) {
+            var mobileNumber = loginuser.MobileNumber;
+            const userRef = db.collection('Users').doc(JSON.stringify(mobileNumber));
+            let checkExistence = await helperObject.checkDocId(mobileNumber);
+            /* Check if the doc id exists in DB */
+            if (checkExistence) {
+                userRef.get().then((snapshot) => {
+                    /* Check if the user exists in DB */
+                    if (snapshot.exists) {
+                        /* Verify the values in DB with input values */
+                        if (snapshot.data().MobileNumber == loginuser.MobileNumber
+                            && snapshot.data().Password == loginuser.Password
+                            && snapshot.data().IsAdmin == loginuser.IsAdmin && loginuser.IsAdmin == true) {
+                            return resolve({ MESSAGE: 'Admin logged in successfully', STATUS: 'SUCCESS' });
+                        } else if (snapshot.data().MobileNumber == loginuser.MobileNumber
+                            && snapshot.data().Password == loginuser.Password
+                            && snapshot.data().IsAdmin == loginuser.IsAdmin && loginuser.IsAdmin == false) {
+                            return resolve({ MESSAGE: 'User logged in successfully', STATUS: 'SUCCESS' });
+                        } else {
+                            return reject({ MESSAGE: 'Please enter a valid credentials', STATUS: 'FAILURE' });
+                        }
                     } else {
-                        return reject({ Message: 'Wrong credentials', Status: 'FAILURE' });
+                        return reject({ MESSAGE: 'Please enter a valid credentials', STATUS: 'FAILURE' });
                     }
-                } else {
-                    return reject({ Message: 'Wrong credentials', Status: 'FAILURE' });
-                }
 
-            });
-        } else {
-            if (request.body.IsAdmin) {
-                reject({ Message: 'Admin not found', Status: 'FAILURE' });
+                });
             } else {
                 reject({ Message: 'User not found', Status: 'FAILURE' });
             }
-
+        } else {
+            return reject({ Message: 'Please enter a valid credentials', Status: 'FAILURE' });
         }
     })
         .then(result => {
--- controller/user-register.js
+++ controller/user-register.js
@@ -2,25 +2,27 @@ var route = require('express').Router();
 var admin = require('firebase-admin');
 var db = admin.firestore();
 var helperObject = require('./helper/helper');
-
+/* API triggered on calling api/user/register  */
 route.post('/register', async (request, response) => {
-    return new Promise( async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         var registerUser = request.body;
         var mobileNumber = registerUser.MobileNumber;
         const userRef = db.collection('Users').doc(JSON.stringify(mobileNumber));
         let checkExistence = await helperObject.checkDocId(mobileNumber);
-        if(!checkExistence) {
-            userRef.set({IsAdmin : false, MobileNumber : mobileNumber, Password : 'password01'});
-            return resolve({ Message: 'Thank you for registering in RVS PayTM', Status: 'SUCCESS' });
+        /* Check if the user already exists or not */
+        if (!checkExistence) {
+            /* Enter the input values to user table in DB */
+            userRef.set({ IsAdmin: false, MobileNumber: mobileNumber, Password: 'password01' });
+            return resolve({ MESSAGE: 'Thank you for registering in RVS PayTM', STATUS: 'SUCCESS' });
         } else {
-            return reject({ Message: 'User already exists', Status: 'FAILURE' });
+            return reject({ MESSAGE: 'User already exists', STATUS: 'FAILURE' });
         }
     }).then(result => {
         response.send(result);
     })
-    .catch(err => {
-        response.send(err);
-    });;
+        .catch(err => {
+            response.send(err);
+        });;
 });
 
 module.exports = route;
