3a07205fad2e0e1c3496a197bd8a3d3ebb0e8c7c
Paul Raju <paul.raju@ADRAPIDVALUE.COM>
2020-01-21 11:57:13 +0530

    work in porgress

--- controller/transaction-history.js
+++ controller/transaction-history.js
@@ -2,157 +2,98 @@ var route = require('express').Router();
 var admin = require('firebase-admin');
 var db = admin.firestore();
 var helperObject = require('./helper/helper');
-/* API triggered on calling api/user/transaction-details */
+var transactionList = [];
+var i = 0;
+/* API triggered on calling api/user/transaction-history */
 route.post('/transaction-history', async (request, response) => {
     return new Promise(async (resolve, reject) => {
         let transactionRequest = request.body;
+        transactionRequest.MobileNumber = JSON.parse(transactionRequest.MobileNumber);
         if (transactionRequest.MobileNumber) {
-            // let checkTransactionExistence = await helperObject.checkTransactionId(transactionRequest.TransactionID);
-            /* Check if the transaction id exists in table */
-            // if (checkTransactionExistence) {
             const transactionRef = db.collection('Transactions');
-            transactionRef.get().then(async (transactionSnap) => {
-                // if (transactionSnap.exists) {
-                if (transactionSnap && transactionSnap.docs) {
-                    var transactionList = [];
-                    transactionSnap.docs.forEach((element) => {
-                        if ((element.data().IsSender && transactionRequest.MobileNumber === element.data().Sender) ||
-                            (!element.data().IsSender && transactionRequest.MobileNumber === element.data().Receiver)) {
-                            // const userRef1 = db.collection('Users').doc(JSON.stringify(transactionRequest.MobileNumber));
+            await transactionRef.get().then((snapshot) => {
+                snapshot.docs.forEach(element => {
+                    if ((transactionRequest.MobileNumber === element.data().Sender) ||
+                        (transactionRequest.MobileNumber === element.data().Receiver)) {
+                            transactionList[i++] = element.data();
+                    }
+                })
+            })
+            resolve(transactionList);
+            // await transactionRef.get().then(async (transactionSnap) => {
+            //     if (transactionSnap && transactionSnap.docs) {
 
+            //         /* Fetch all the items in transaction table */
+            //         transactionSnap.docs.forEach((element) => {
+            //             /* Check in each item whether the requested phone number is involved */
+            //             if ((transactionRequest.MobileNumber === element.data().Sender) ||
+            //                 (transactionRequest.MobileNumber === element.data().Receiver)) {
 
+            //                 /* Fetch the details of the user */
+            //                 const userRef1 = db.collection('Users').doc(JSON.stringify(transactionRequest.MobileNumber));
+            //                 userRef1.get().then((userSnap1) => {
+            //                     if (userSnap1.exists) {
+            //                         var user1 = userSnap1.data();
+            //                         var details = {
+            //                             UserName: null,
+            //                             UserNumber: null,
+            //                             UserBalance: null,
+            //                             OtherUserName: null,
+            //                             OtherUserNumber: null,
+            //                             TransactionDate: null,
+            //                             TransactionID: null,
+            //                             IsSender: false
+            //                         };
 
+            //                         if (element.data().IsSender) {
+            //                             details.UserName = user1.MobileNumber;
+            //                             details.UserNumber = user1.MobileNumber;
+            //                             details.UserBalance = user1.AccountBalance;
+            //                             details.OtherUserName = element.data().Receiver;
+            //                             details.OtherUserNumber = element.data().Receiver;
+            //                         } else {
+            //                             details.UserName = element.data().Sender;
+            //                             details.UserNumber = element.data().Sender;
+            //                             details.UserBalance = user1.AccountBalance;
+            //                             details.OtherUserName = user1.MobileNumber;
+            //                             details.OtherUserNumber = user1.MobileNumber;
+            //                         }
+            //                         details.TransactionDate = element.data().TransactionDate;
+            //                         details.TransactionID = element.data().TransactionID;
+            //                         details.IsSender = element.data().IsSender;
 
-                            const userRef1 = db.collection('Users').doc(JSON.stringify(element.data().Sender));
-                            userRef1.get().then((userSnap1) => {
-                                if (userSnap1.exists) {
-                                    var user1 = userSnap1.data();
-                                    /* Fetch the receiver details from users table */
-                                    const userRef2 = db.collection('Users').doc(JSON.stringify(element.data().Receiver));
-                                    userRef2.get().then((userSnap2) => {
-                                        if (userSnap2.exists) {
-                                            var user2 = userSnap2.data();
-                                            /* Combine the sender and receiver details into one single object and send back to details api */
-                                            var details = {
-                                                UserName: null,
-                                                UserNumber: null,
-                                                UserBalance: null,
-                                                OtherUserName: null,
-                                                OtherUserNumber: null,
-                                                TransactionDate: null,
-                                                TransactionID: null,
-                                                IsSender: false
-                                            };
+            //                         transactionList.push(details);
 
-                                            if (element.data().IsSender) {
-                                                details.UserName = user1.MobileNumber;
-                                                details.UserNumber = user1.MobileNumber;
-                                                details.UserBalance = user1.AccountBalance;
-                                                details.OtherUserName = user2.MobileNumber;
-                                                details.OtherUserNumber = user2.MobileNumber;                                                
-                                            } else {
-                                                details.UserName = user2.MobileNumber;
-                                                details.UserNumber = user2.MobileNumber;
-                                                details.UserBalance = user2.AccountBalance;
-                                                details.OtherUserName = user1.MobileNumber;
-                                                details.OtherUserNumber = user1.MobileNumber;
-                                            }
-                                            details.TransactionDate = element.data().TransactionDate;
-                                            details.TransactionID = element.data().TransactionID;
-                                            details.IsSender = element.data().IsSender;
-                                            // db.collection('Transactions').doc('2').set({Amount: 0,IsSender: false,Receiver:9020777509,Sender:9999999999,TransactionDate:"",TransactionID:2,UserNumber:9020777509});
-                                            transactionList.push(details);
-
-                                        } else {
-                                            return reject({ Message: 'Invalid request', Status: 'FAILURE' });
-                                        }
-                                        return resolve({ Message: 'Details fetched successfuly', Data: transactionList, Status: 'SUCCESS' });
-                                    });
-                                } else {
-                                    return reject({ Message: 'Invalid request', Status: 'FAILURE' });
-                                }
-                            });
-                        }
-
-
-
-
-                    });
-                }
-
-
-
-
-
-
-
-                // if ((transactionSnap.data().IsSender && transactionRequest.MobileNumber === transactionSnap.data().Sender) ||
-                //     (!transactionSnap.data().IsSender && transactionRequest.MobileNumber === transactionSnap.data().Receiver)) {
-                /* Fetch the sender details from users table */
-                // const userRef1 = db.collection('Users').doc(JSON.stringify(transactionSnap.data().Sender));
-                // userRef1.get().then((userSnap1) => {
-                //     if (userSnap1.exists) {
-                //         var user1 = userSnap1.data();
-                //         /* Fetch the receiver details from users table */
-                //         const userRef2 = db.collection('Users').doc(JSON.stringify(transactionSnap.data().Receiver));
-                //         userRef2.get().then((userSnap2) => {
-                //             if (userSnap2.exists) {
-                //                 var user2 = userSnap2.data();
-                //                 /* Combine the sender and receiver details into one single object and send back to details api */
-                //                 var details = {
-                //                     UserName: null,
-                //                     UserNumber: null,
-                //                     UserBalance: null,
-                //                     OtherUserName: null,
-                //                     OtherUserNumber: null,
-                //                     TransactionDate: null,
-                //                     TransactionID: null
-                //                 };
-
-                //                 if (transactionSnap.data().IsSender) {
-                //                     details.UserName = user1.MobileNumber;
-                //                     details.UserNumber = user1.MobileNumber;
-                //                     details.UserBalance = user1.AccountBalance;
-                //                     details.OtherUserName = user2.MobileNumber;
-                //                     details.OtherUserNumber = user2.MobileNumber;
-                //                 } else {
-                //                     details.UserName = user2.MobileNumber;
-                //                     details.UserNumber = user2.MobileNumber;
-                //                     details.UserBalance = user2.AccountBalance;
-                //                     details.OtherUserName = user1.MobileNumber;
-                //                     details.OtherUserNumber = user1.MobileNumber;
-                //                 }
-                //                 details.TransactionDate = transactionSnap.data().TransactionDate;
-                //                 details.TransactionID = transactionSnap.data().TransactionID;
-                //                 // db.collection('Transactions').doc('2').set({Amount: 0,IsSender: false,Receiver:9020777509,Sender:9999999999,TransactionDate:"",TransactionID:2,UserNumber:9020777509});
-                //                 return resolve({ Message: 'Details fetched successfuly', Data: details, Status: 'SUCCESS' });
-                //             } else {
-                //                 return reject({ Message: 'Invalid request', Status: 'FAILURE' });
-                //             }
-
-                //         });
-                //     } else {
-                //         return reject({ Message: 'Invalid request', Status: 'FAILURE' });
-                //     }
-                // });
-                // } else {
-                //     return reject({ Message: 'Invalid request', Status: 'FAILURE' });
-                // }
-
-                // }
-            });
-            // } else {
-            //     return reject({ Message: 'Invalid request', Status: 'FAILURE' });
-            // }
+            //                         // if (transactionSnap.docs.length === transactionList.length) {
+            //                         //     return resolve({ Message: 'Details fetched successfuly', Data: transactionList, Status: 'SUCCESS' });
+            //                         // }
+            //                     } else {
+            //                         return reject({ Message: 'Invalid requestaaa', Status: 'FAILURE' });
+            //                     }
+            //                 });
+            //             } 
+            //             // else {
+            //             //     return reject({ Message: 'Invalid requestbbb', Status: 'FAILURE' });
+            //             // }
+            //         });
+            //     }
+            // });
+            return resolve({ Message: 'Details fetched successfuly', Data: transactionList, Status: 'SUCCESS' });
         } else {
             return reject({ Message: 'Invalid request', Status: 'FAILURE' });
         }
     })
         .then(result => {
-            response.send(result);
+            if (transactionList == '') {
+                response.send({ Message: 'No Transactions yet', Data: [], Status: 'SUCCESS' });
+            }
+            else {
+                response.send({ Message: 'Details fetched successfuly', Data: result, Status: 'SUCCESS' });
+                transactionList = [];
+            }
         })
         .catch(err => {
-            response.send(err);
+            response.send({ Message: 'No Transactions yet', Data: result, Status: 'SUCCESS' });
         });
 });
 
