8f5a0e59de6bb84f85dda3b7a101bbee71f25738
Paul Raju <paul.raju@ADRAPIDVALUE.COM>
2020-01-21 14:50:35 +0530

    working

--- controller/transaction-history.js
+++ controller/transaction-history.js
@@ -4,6 +4,7 @@ var db = admin.firestore();
 var helperObject = require('./helper/helper');
 var transactionList = [];
 var i = 0;
+var count = 0;
 /* API triggered on calling api/user/transaction-history */
 route.post('/transaction-history', async (request, response) => {
     return new Promise(async (resolve, reject) => {
@@ -11,15 +12,52 @@ route.post('/transaction-history', async (request, response) => {
         transactionRequest.MobileNumber = JSON.parse(transactionRequest.MobileNumber);
         if (transactionRequest.MobileNumber) {
             const transactionRef = db.collection('Transactions');
-            await transactionRef.get().then((snapshot) => {
-                snapshot.docs.forEach(element => {
-                    if ((transactionRequest.MobileNumber === element.data().Sender) ||
-                        (transactionRequest.MobileNumber === element.data().Receiver)) {
-                            transactionList[i++] = element.data();
+            await transactionRef.get().then((transactionSnap) => {
+                transactionSnap.docs.forEach(element => {
+                    if ((element.data().IsSender && transactionRequest.MobileNumber === element.data().Sender) ||
+                        (!element.data().IsSender && transactionRequest.MobileNumber === element.data().Receiver)) {
+                            count++;
+                             resolve({ Message: 'Details fetched successfuly', Data: count, Status: 'SUCCESS' });
+                        /* Fetch the details of the user */
+                        const userRef1 = db.collection('Users').doc(JSON.stringify(transactionRequest.MobileNumber));
+                        userRef1.get().then((userSnap1) => {
+                            var details = {
+                                UserName: null,
+                                UserNumber: null,
+                                UserBalance: null,
+                                OtherUserName: null,
+                                OtherUserNumber: null,
+                                TransactionDate: null,
+                                TransactionID: null,
+                                IsSender: false
+                            };
+                            if (element.data().IsSender) {
+                                details.UserName = userSnap1.data().MobileNumber;
+                                details.UserNumber = userSnap1.data().MobileNumber;
+                                details.UserBalance = userSnap1.data().AccountBalance;
+                                details.OtherUserName = element.data().Receiver;
+                                details.OtherUserNumber = element.data().Receiver;
+                            } else {
+                                details.UserName = element.data().Sender;
+                                details.UserNumber = element.data().Sender;
+                                details.UserBalance = userSnap1.data().AccountBalance;
+                                details.OtherUserName = userSnap1.data().MobileNumber;
+                                details.OtherUserNumber = userSnap1.data().MobileNumber;
+                            }
+                            details.TransactionDate = element.data().TransactionDate;
+                            details.TransactionID = element.data().TransactionID;
+                            details.IsSender = element.data().IsSender;
+                            transactionList[i++] = details;
+                        });
+
+
                     }
                 })
             })
-            resolve(transactionList);
+            if(transactionList.length === count){
+                resolve(transactionList);
+            }
+            
             // await transactionRef.get().then(async (transactionSnap) => {
             //     if (transactionSnap && transactionSnap.docs) {
 
@@ -78,19 +116,19 @@ route.post('/transaction-history', async (request, response) => {
             //         });
             //     }
             // });
-            return resolve({ Message: 'Details fetched successfuly', Data: transactionList, Status: 'SUCCESS' });
+            // return resolve({ Message: 'Details fetched successfuly', Data: transactionList, Status: 'SUCCESS' });
         } else {
             return reject({ Message: 'Invalid request', Status: 'FAILURE' });
         }
     })
         .then(result => {
-            if (transactionList == '') {
-                response.send({ Message: 'No Transactions yet', Data: [], Status: 'SUCCESS' });
-            }
-            else {
-                response.send({ Message: 'Details fetched successfuly', Data: result, Status: 'SUCCESS' });
-                transactionList = [];
-            }
+            // if (transactionList == '') {
+                response.send({ Message: 'No Transactions yet', Data: result, Status: 'SUCCESS' });
+            // }
+            // else {
+            //     response.send({ Message: 'Details fetched successfuly', Data: result, Status: 'SUCCESS' });
+            //     transactionList = [];
+            // }
         })
         .catch(err => {
             response.send({ Message: 'No Transactions yet', Data: result, Status: 'SUCCESS' });
